01e2eff441c7c61ea48a080dc2598bbe978e9851
Natnael Alemu <natnaelalemu1998@gmail.com>
2023-03-28 14:48:17 +0300

    created spring boot project

---
+++ src/main/java/com/actproject/actproject/ActprojectApplication.java
@@ -0,0 +1,13 @@
+package com.actproject.actproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActprojectApplication.class, args);
+    }
+
+}
